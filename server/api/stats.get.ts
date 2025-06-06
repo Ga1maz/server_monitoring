8949ceea540c750b0e08e549758db05522307039
Ga1maz <aganiev806@gmail.com>
2025-06-06 14:30:32 +0500

    UpDate

--- server/api/stats.get.ts
+++ server/api/stats.get.ts
@@ -1,20 +1,19 @@
-// server/api/stats.get.ts
 import { execSync } from 'child_process'
 
 export default defineEventHandler(() => {
-  const memInfo = execSync("free -g | awk '/Mem:/ { print $2, $3 }'").toString().trim().split(' ')
-  const diskInfo = execSync("df -BG --output=size,used / | tail -1").toString().trim().split(' ')
+  const memInfo = execSync("free -m | awk '/Mem:/ { print $2, $3 }'").toString().trim().split(' ')
+  const diskInfo = execSync("df -BM --output=size,used / | tail -1").toString().trim().split(/\s+/)
   const cpuRaw = execSync("mpstat -P ALL 1 1 | awk '/^[0-9]/ && $3 ~ /^[0-9]+$/ { print 100 - $12 }'").toString().trim().split('\n')
   const uptime = execSync("uptime -p").toString().trim()
 
   return {
     memory: {
-      total: parseInt(memInfo[0]),
-      used: parseInt(memInfo[1])
+      total: parseFloat((parseInt(memInfo[0]) / 1024).toFixed(1)),
+      used: parseFloat((parseInt(memInfo[1]) / 1024).toFixed(1))
     },
     disk: {
-      total: parseInt(diskInfo[0]),
-      used: parseInt(diskInfo[1])
+      total: parseFloat((parseInt(diskInfo[0]) / 1024).toFixed(1)),
+      used: parseFloat((parseInt(diskInfo[1]) / 1024).toFixed(1))
     },
     threads: cpuRaw.slice(0, 16).map(v => parseFloat(parseFloat(v).toFixed(1))),
     uptime
